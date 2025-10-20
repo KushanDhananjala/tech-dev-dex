23c8f7402ddee6a5ce8cd85add488b93a0572b46
KushanDhananjala <kushan.xtream@gmail.com>
2025-10-20 12:41:12 +0530

    fix favicon issues

--- src/components/SEO.tsx
+++ src/components/SEO.tsx
@@ -129,8 +129,7 @@ export default function SEO({
       {/* Favicon */}
       <link rel="icon" href="/favicon.ico" sizes="any" />
       <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
-      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+      <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png" />
       <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
       <link rel="manifest" href="/site.webmanifest" />
       <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#3B82F6" />
